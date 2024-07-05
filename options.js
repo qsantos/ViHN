d6dd00514ddf720a3ed876a176f4381ceb32a39b
Quentin Santos <qsantos@qsantos.fr>
2024-07-05 08:57:47 +0200

    Apply automatic formatting

--- options.js
+++ options.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    chrome.storage.sync.get(options => {
+    chrome.storage.sync.get((options) => {
         function onChange(event) {
             const input = event.target;
             options[input.name] = input.checked;
             chrome.storage.sync.set(options);
         }
 
-        for (const input of document.getElementsByTagName('input')) {
+        for (const input of document.getElementsByTagName("input")) {
             input.checked = getOption(options, input.name);
             input.addEventListener("input", onChange);
         }
