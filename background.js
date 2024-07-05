526e3403b34e634eb513fe0648d95cf43e69e2ca
Quentin Santos <qsantos@qsantos.fr>
2024-07-05 08:40:38 +0200

    Use strict equality everywhere

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener((message, sender) => {
-    if (message.action == 'open') {
+    if (message.action === 'open') {
         chrome.tabs.create({
             active: message.active,
             url: message.url,
